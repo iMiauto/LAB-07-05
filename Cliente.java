dfce7879cbd8a589ceb2d6d39a0fbeb679313a00
dsanchezGIT <sanchezad@gmail.com>
2025-05-07 20:07:54 -0600

    cambio profe

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import java.io.*;
 import java.net.*;
 
@@ -25,4 +23,3 @@ public class Cliente {// se crea la clase cliente
 }// fin de la clase
 
 // hola mundo
->>>>>>> bf818f53037d1c2594cd495a84728df3e19fe89d
